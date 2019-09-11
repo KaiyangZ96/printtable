5260146fac1b3d82b9d24741f2af2294e2054f1b
ZKY <zky@n187-p173.eduroam.kth.se>
2019-09-11 15:01:50 +0200

    added function

--- printtable.py
+++ printtable.py
@@ -1,5 +1,8 @@
 def multtable(number, limit):
     for i in range(1, limit+1):
         print(i * number)
+def powertable(number,b):
+    for i in range(1,b+1):
+        result = i ** number
+        print(result)
 
-multtable(7, 10)
